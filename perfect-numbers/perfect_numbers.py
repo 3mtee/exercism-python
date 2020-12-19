afeebf849847b52957a0247761ba8547f4f5f3c6
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-19 22:47:27 +0200

    Improved the  "Perfect Numbers" solution
    Introduced `map()` to get the factors of a number

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -4,7 +4,7 @@ from functools import reduce
 def classify(number):
     if number < 1:
         raise ValueError("You can't pass number less than 1")
-    factors = get_factors(number)
+    factors = map(lambda i: i if number % i == 0 else 0, range(1, number))
     aliquot = reduce(lambda x, y: x + y, factors, 0)
 
     if aliquot == number:
@@ -13,11 +13,3 @@ def classify(number):
         return "abundant"
     else:
         return "deficient"
-
-
-def get_factors(number):
-    factors = []
-    for i in range(1, number):
-        if number % i == 0:
-            factors.append(i)
-    return factors
