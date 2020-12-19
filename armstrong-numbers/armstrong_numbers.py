aa26c37f4250384a7bbad7daea47f9d8fec4dd98
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-19 18:25:29 +0200

    Implemented solution for the "Armstrong Numbers"

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,7 @@
 def is_armstrong_number(number):
-    pass
+    digits = [int(i) for i in str(number)]
+    d_len = len(digits)
+    d_sum = 0
+    for digit in digits:
+        d_sum += digit ** d_len
+    return d_sum == number
