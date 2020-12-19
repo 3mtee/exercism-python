953a159c38fd29ce5c363fc195314db9f85a342c
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-19 17:57:47 +0200

    Updated a "High Scores" solution

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,14 +1,12 @@
 def latest(scores):
-    return scores[len(scores) - 1]
+    return scores[-1]
 
 
 def personal_best(scores):
-    sorted_scores = scores.copy()
-    sorted_scores.sort(reverse=True)
+    sorted_scores = sorted(scores, reverse=True)
     return sorted_scores[0]
 
 
 def personal_top_three(scores):
-    sorted_scores = scores.copy()
-    sorted_scores.sort(reverse=True)
+    sorted_scores = sorted(scores, reverse=True)
     return sorted_scores[0:3]
