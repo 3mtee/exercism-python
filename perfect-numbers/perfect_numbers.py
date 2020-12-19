bbc613b79a4fec1bf1860de972a9f9431c71f0fe
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-19 22:40:00 +0200

    Implemented the  "Perfect Numbers" solution

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,2 +1,23 @@
+from functools import reduce
+
+
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError("You can't pass number less than 1")
+    factors = get_factors(number)
+    aliquot = reduce(lambda x, y: x + y, factors, 0)
+
+    if aliquot == number:
+        return "perfect"
+    elif aliquot > number:
+        return "abundant"
+    else:
+        return "deficient"
+
+
+def get_factors(number):
+    factors = []
+    for i in range(1, number):
+        if number % i == 0:
+            factors.append(i)
+    return factors
