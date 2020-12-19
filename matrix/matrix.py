7e28df8de788885935f3c4a2a08df8e80aef10a0
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-19 19:15:10 +0200

    Improved readability of the "Matrix"

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,11 @@
 class Matrix:
-    def __init__(self, matrix_string):
-        pass
+    def __init__(self, matrix_string: str):
+        lines = matrix_string.splitlines()
+        splitlines_ = [list(map(int, line.split())) for line in lines]
+        self.__matrix = splitlines_
 
     def row(self, index):
-        pass
+        return self.__matrix[index - 1]
 
     def column(self, index):
-        pass
+        return [row[index - 1] for row in self.__matrix]
