21929910df9b61c0b927ebee7f8cf521b7a8fc74
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-27 15:48:11 +0200

    Implemented a "Series" solution

--- series/series.py
+++ series/series.py
@@ -1,2 +1,6 @@
 def slices(series, length):
-    pass
+    series_length = len(series)
+    if length > series_length or length < 1:
+        raise ValueError("Incorrect length was requested")
+
+    return [series[a:a + length] for a in range(0, series_length - length + 1)]
