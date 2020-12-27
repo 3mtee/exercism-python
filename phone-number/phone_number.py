d9608a402f0b449d91d17b36cdb1957631a1cdb7
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-27 16:54:41 +0200

    Implemented a "Phone Number" solution

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,3 +1,22 @@
+import re
+
+
 class PhoneNumber:
     def __init__(self, number):
-        pass
+        self.number = self.clear_number(number)
+        self.area_code = self.number[0:3]
+        wrong_length = len(self.number) > 10
+        wrong_n = int(self.number[0]) < 2 or int(self.number[3]) < 2
+        if wrong_length or wrong_n:
+            raise ValueError("Incorrect input")
+
+    def pretty(self):
+        return f"({self.number[0:3]})-{self.number[3:6]}-{self.number[6:]}"
+
+    @staticmethod
+    def clear_number(number: str):
+        parts = re.findall(r"\d+", number)
+        num = "".join(parts)
+        if len(num) == 11 and num[0] == "1":
+            return num[1:]
+        return num
