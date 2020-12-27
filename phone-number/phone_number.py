ac1dfc28b87068d4a7ca757bc9da60ad5ed5d4f9
3mtee <731529+3mtee@users.noreply.github.com>
2020-12-27 17:23:42 +0200

    Refined a "Phone Number" solution

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -2,21 +2,22 @@ import re
 
 
 class PhoneNumber:
+    _pattern = re.compile(r"""
+           ^\+?1?                   #country
+           [-. (]*([2-9]\d{2})\)?   #area
+           [-. (]*([2-9]\d{2})\)?   #exchange
+           [-. (]*(\d{4})\)?$       #subscriber
+           """, re.VERBOSE)
+
     def __init__(self, number):
-        self.number = self.clear_number(number)
-        self.area_code = self.number[0:3]
-        wrong_length = len(self.number) > 10
-        wrong_n = int(self.number[0]) < 2 or int(self.number[3]) < 2
-        if wrong_length or wrong_n:
-            raise ValueError("Incorrect input")
+        try:
+            self.area_code, self.exchange, self.subscriber = self._pattern.search(number.strip()).groups()
+        except AttributeError:
+            raise ValueError("Invalid input")
+
+    @property
+    def number(self):
+        return self.area_code + self.exchange + self.subscriber
 
     def pretty(self):
-        return f"({self.number[0:3]})-{self.number[3:6]}-{self.number[6:]}"
-
-    @staticmethod
-    def clear_number(number: str):
-        parts = re.findall(r"\d+", number)
-        num = "".join(parts)
-        if len(num) == 11 and num[0] == "1":
-            return num[1:]
-        return num
+        return f"({self.area_code})-{self.exchange}-{self.subscriber}"
