e2f99b08179b392d1b4151fe75f1e900a3b1928a
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-04 17:46:04 +0200

    Implemented the "Hamming" exercise solution

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,4 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands should have equal length")
+    return len([i for i, j in zip(strand_a, strand_b) if i != j])
