a96931292e6d7a694e23a7461a43ae8b5bb7246f
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-05 16:02:57 +0200

    Refined the "ISBN Verifier" exercise solution

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,19 +1,17 @@
 import re
-import functools
-import operator
+
+
+def check_isbn_formula(digits):
+    numbers_to_mult = list(map(int, digits))
+    return sum([factors[0] * factors[1] for factors in zip(numbers_to_mult, range(10, 0, -1))]) % 11 == 0
 
 
 def is_valid(isbn):
-    pattern = re.compile(r"(?:(\d+)(?:-)?(X)?)")
-    tup = pattern.findall(isbn)
-
-    refined = "".join([a + b for a, b in tup])
-    if len(refined) != 10 or ("X" in refined and refined.index("X") != 9):
+    stripped_isbn = isbn.replace("-", "")
+    if not re.compile("[0-9]{9}([0-9]|X)$").match(stripped_isbn):
         return False
-    i = 10
-    res = []
-    for r in refined:
-        n = 10 if r == "X" else int(r)
-        res.append(n * i)
-        i = i - 1
-    return functools.reduce(operator.add, res) % 11 == 0
+    else:
+        digits = list(stripped_isbn)
+        if digits[-1] == 'X':
+            digits[-1] = '10'
+        return check_isbn_formula(digits)
