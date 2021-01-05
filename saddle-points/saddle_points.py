9b6aac74011236ca952509c0ae04f24956056b14
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-05 19:06:24 +0200

    Implemented draft of the "Saddle Points" exercise

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,2 +1,14 @@
 def saddle_points(matrix):
-    pass
+    saddles = []
+
+    for row_num, row in enumerate(matrix):
+        saddle = max(row)
+        saddles += [{"row": row_num + 1, "column": i + 1, "value": s} for i, s in enumerate(row) if s == saddle]
+
+    for i, saddle in enumerate(saddles.copy()):
+        col_num = saddle["column"] - 1
+        for row in matrix:
+            if row[col_num] < saddle["value"]:
+                saddles[i] = None
+    return [{"row": s["row"], "column": s["column"]} for s in saddles if s]
+
