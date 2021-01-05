a36d8dff94ad2f718d786eace8b843801d531328
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-05 16:29:50 +0200

    Refined the "Anagram" exercise solution

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,4 +1,8 @@
 def find_anagrams(word: str, candidates):
+    return [c for c in candidates if is_anagram(word, c)]
+
+
+def is_anagram(word: str, candidate: str):
     l_word = word.lower()
-    l_candidates = [c.lower() for c in candidates]
-    return [c for c, l in zip(candidates, l_candidates) if sorted(l) == sorted(l_word) and l != l_word]
+    l_candidate = candidate.lower()
+    return sorted(l_candidate) == sorted(l_word) and l_candidate != l_word
