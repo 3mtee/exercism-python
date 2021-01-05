5ed5b66335d3c8aa1fe0371f45068806d7dbab99
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-05 10:15:46 +0200

    Solved the "ISBN Verifier" exercise

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,19 @@
+import re
+import functools
+import operator
+
+
 def is_valid(isbn):
-    pass
+    pattern = re.compile(r"(?:(\d+)(?:-)?(X)?)")
+    tup = pattern.findall(isbn)
+
+    refined = "".join([a + b for a, b in tup])
+    if len(refined) != 10 or ("X" in refined and refined.index("X") != 9):
+        return False
+    i = 10
+    res = []
+    for r in refined:
+        n = 10 if r == "X" else int(r)
+        res.append(n * i)
+        i = i - 1
+    return functools.reduce(operator.add, res) % 11 == 0
