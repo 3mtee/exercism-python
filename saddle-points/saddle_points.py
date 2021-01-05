ea87abbdb7549b768c617e3781e74435cb12369f
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-05 19:14:08 +0200

    Implemented the "Saddle Points" exercise

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,8 +1,15 @@
 def saddle_points(matrix):
-    saddles = []
+    saddles: list[map] = []
 
+    row_len = None
     for row_num, row in enumerate(matrix):
         saddle = max(row)
+        if row_num == 0:
+            row_len = len(row)
+
+        if len(row) != row_len:
+            raise ValueError("Irregular matrices are forbidden")
+
         saddles += [{"row": row_num + 1, "column": i + 1, "value": s} for i, s in enumerate(row) if s == saddle]
 
     for i, saddle in enumerate(saddles.copy()):
