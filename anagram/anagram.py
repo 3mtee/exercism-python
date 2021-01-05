c88e786ad07a89367a3e75a37b3d2b482ec90c42
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-05 16:25:06 +0200

    Finished the "Anagram" exercise

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,4 @@
-def find_anagrams(word, candidates):
-    pass
+def find_anagrams(word: str, candidates):
+    l_word = word.lower()
+    l_candidates = [c.lower() for c in candidates]
+    return [c for c, l in zip(candidates, l_candidates) if sorted(l) == sorted(l_word) and l != l_word]
