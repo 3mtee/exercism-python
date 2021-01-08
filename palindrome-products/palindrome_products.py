e120232f95ad34bfbd02c366a3b90907b67cc344
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-08 09:34:48 +0200

    Refined the "Palindrome Products" exercise solution

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -1,36 +1,38 @@
-def largest(min_factor: int, max_factor: int):
-    if min_factor > max_factor:
+def smallest(max_factor, min_factor):
+    return is_palindrome(min_factor, max_factor)
+
+
+def largest(max_factor, min_factor):
+    return is_palindrome(min_factor, max_factor, low=False)
+
+
+def is_palindrome(mn, mx, low=True):
+    """
+    Throughout the method all ranges' upper bound is extended because in python it's exclusive
+    and we need to include it.
+    :param mn: min value
+    :param mx: max value
+    :param low: True means we need smallest palindrome, False - largest one
+    :return:
+    """
+    if mn > mx:
         raise ValueError("min shouldn't be bigger than max")
-    value = None
-    factors = set()
-    for i in range(max_factor, min_factor - 1, -1):
-        for j in range(max_factor, min_factor - 1, -1):
-            multiplication = i * j
-            palindrome = is_palindrome(multiplication)
-            if palindrome and (not value or multiplication > value):
-                value = multiplication
-                factors.clear()
-            if multiplication == value:
-                factors.add((i, j))
-    return value, factors
+    # iterate over range of squares to avoid nested loops
+    args = (mn ** 2, mx ** 2 + 1) if low else (mx ** 2, mn ** 2 - 1, -1)
 
+    for r in range(*args):
+        s = str(r)
+        palindrome = s == s[::-1]
+        if palindrome:
+            # Since we're iterating through range created using smallest and largest squares as its bounds,
+            # it'll contain numbers which aren't products of the min-max range.
+            # So we need to check if there's a factor of a palindrome in the initial range
+            # mn <= r // j <= mx is here because we extended our ranges by 1
+            factor_in_range = any(mn <= r // j <= mx for j in range(mn, mx + 1) if r % j == 0)
+            if factor_in_range:
+                # Similar to the above we need to find the factors of a palindrome in the initial range
+                return r, ((i, r // i) for i in range(mn, mx + 1)
+                           if r % i == 0 and mn <= i <= r // i <= mx)
+    else:
+        return None, []
 
-def smallest(min_factor: int, max_factor: int):
-    if min_factor > max_factor:
-        raise ValueError("min shouldn't be bigger than max")
-    value = None
-    factors = set()
-    for i in range(min_factor, max_factor + 1):
-        for j in range(min_factor, max_factor + 1):
-            multiplication = i * j
-            palindrome = is_palindrome(multiplication)
-            if palindrome and (not value or multiplication < value):
-                value = multiplication
-                factors.clear()
-            if multiplication == value:
-                factors.add((i, j))
-    return value, factors
-
-
-def is_palindrome(number: int):
-    return str(number)[::-1] == str(number)
