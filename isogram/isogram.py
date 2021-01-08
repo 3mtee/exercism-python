21a9dee5ebc67297683c2051afa1b9a03cb1e928
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-08 16:55:12 +0200

    Did the "Isogram" exercise

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,6 @@
-def is_isogram(string):
-    pass
+import re
+
+
+def is_isogram(string: str):
+    clean_string = re.sub("[^a-z]", "", string.lower())
+    return len(set(clean_string)) == len(clean_string)
