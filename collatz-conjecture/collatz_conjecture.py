8018a92f37887058ea146b3e82a0826adae34445
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-08 16:28:52 +0200

    Did the "Collatz Conjecture" exercise

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Number should be a positive one")
+    return __calc_steps(number, 0)
+
+
+def __calc_steps(number, step):
+    if number == 1:
+        return step
+    if number % 2 == 0:
+        return __calc_steps(number / 2, step + 1)
+    else:
+        return __calc_steps(number * 3 + 1, step + 1)
