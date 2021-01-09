1604f9bab60dccc93c6d8b2a0592472796247b23
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-09 18:39:27 +0200

    Did the "Prime Factors" exercise

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,24 @@
 def factors(value):
-    pass
+    results = []
+    i = 2
+    num = value
+    while num != 1:
+        if num % i == 0:
+            results.append(i)
+            num = num / i
+        else:
+            i += 1
+    return results
+
+
+def is_prime(n: int) -> bool:
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
