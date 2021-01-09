9f1dc664fff02689e18f7474fbda3dc9ecbff7bf
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-09 18:24:59 +0200

    Did the "Grains" exercise

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,8 @@
 def square(number):
-    pass
+    if number <= 0 or number > 64:
+        raise ValueError("Number should be between 1 and 64")
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    return sum(square(n) for n in range(1, 65))
