1ec50c59165be89b10ed84f7b137bd6beabb9716
3mtee <731529+3mtee@users.noreply.github.com>
2021-01-11 11:31:02 +0200

    Refined the "Yacht" exercise

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -1,49 +1,22 @@
 from collections import Counter
 
-"""
-This exercise stub and the test suite contain several enumerated constants.
-
-Since Python 2 does not have the enum module, the idiomatic way to write
-enumerated constants has traditionally been a NAME assigned to an arbitrary,
-but unique value. An integer is traditionally used because it’s memory
-efficient.
-It is a common practice to export both constants and functions that work with
-those constants (ex. the constants in the os, subprocess and re modules).
-
-You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
-"""
-
-# Score categories.
-# Change the values as you see fit.
-YACHT = 0
-ONES = 1
-TWOS = 2
-THREES = 3
-FOURS = 4
-FIVES = 5
-SIXES = 6
-FULL_HOUSE = 7
-FOUR_OF_A_KIND = 8
-LITTLE_STRAIGHT = 9
-BIG_STRAIGHT = 10
-CHOICE = 11
+YACHT = (lambda x: 50 if len(set(x)) == 1 else 0)
+ONES = (lambda x: __count_dices(x, 1))
+TWOS = (lambda x: __count_dices(x, 2))
+THREES = (lambda x: __count_dices(x, 3))
+FOURS = (lambda x: __count_dices(x, 4))
+FIVES = (lambda x: __count_dices(x, 5))
+SIXES = (lambda x: __count_dices(x, 6))
+FULL_HOUSE = (lambda x: sum(x) if sorted(Counter(x).values()) == [2, 3] else 0)
+FOUR_OF_A_KIND = (lambda x: sum(k * 4 for k, v in Counter(x).items() if v >= 4))
+LITTLE_STRAIGHT = (lambda x: 30 if sorted(x) == [1, 2, 3, 4, 5] else 0)
+BIG_STRAIGHT = (lambda x: 30 if sorted(x) == [2, 3, 4, 5, 6] else 0)
+CHOICE = sum
 
 
 def score(dice, category):
-    if category == YACHT:
-        return 50 if len(set(dice)) == 1 else 0
-    if category in (ONES, TWOS, THREES, FOURS, FIVES, SIXES):
-        return sum(i for i in dice if i == category)
-    if category == FULL_HOUSE:
-        counts = Counter(dice)
-        return sum(k * v for k, v in counts.items()) if sorted(list(counts.values())) == [2, 3] else 0
-    if category == FOUR_OF_A_KIND:
-        counts = Counter(dice)
-        return sum(k * 4 for k, v in counts.items() if v >= 4)
-    if category == LITTLE_STRAIGHT:
-        return 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0
-    if category == BIG_STRAIGHT:
-        return 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0
-    if category == CHOICE:
-        return sum(dice)
-    return 0
+    return category(dice)
+
+
+def __count_dices(x: list, digit):
+    return sum(i for i in x if i == digit)
