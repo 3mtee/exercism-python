13acfdbb937135d0723d69ada5b4a9d88c743e2d
3mtee <731529+3mtee@users.noreply.github.com>
2021-02-04 13:05:18 +0200

    Refined the "Isogram" solution

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(string: str):
-    clean_string = "".join(c.lower() for c in string if c.isalpha())
+    clean_string = "".join(c for c in string.lower() if c.isalpha())
     return len(set(clean_string)) == len(clean_string)
