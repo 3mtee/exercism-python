5f15c7d53e7f5314a8114ca717123bcbdd3777cf
3mtee <731529+3mtee@users.noreply.github.com>
2021-02-08 16:11:47 +0200

    Did the "Meetup" exercise

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -1,2 +1,26 @@
+import calendar
+
+
 def meetup(year, month, week, day_of_week):
-    pass
+    week_days = list(calendar.day_name)
+    c = calendar.Calendar()
+
+    days = [d for d in c.itermonthdates(year, month) if
+            d.weekday() == week_days.index(day_of_week) and d.month == month]
+
+    if week == 'teenth':
+        result = [d for d in days if 13 <= d.day <= 19][0]
+    elif week == 'last':
+        result = days[-1]
+    else:
+        try:
+            result = days[int(week[0]) - 1]
+        except IndexError:
+            raise MeetupDayException("Incorrect meetup instructions")
+
+    return result
+
+
+class MeetupDayException(Exception):
+    def __init__(self, *args):
+        super().__init__(args)
