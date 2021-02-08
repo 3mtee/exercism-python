15308da6428a88f7efe5ece26c2e650eae0daa22
3mtee <731529+3mtee@users.noreply.github.com>
2021-02-08 13:51:28 +0200

    Did the "Allergies" exercise

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,11 +1,22 @@
 class Allergies:
+    allergens = {
+        "eggs": 1,
+        "peanuts": 2,
+        "shellfish": 4,
+        "strawberries": 8,
+        "tomatoes": 16,
+        "chocolate": 32,
+        "pollen": 64,
+        "cats": 128
+    }
 
     def __init__(self, score):
-        pass
+        self.score = score
 
     def allergic_to(self, item):
-        pass
+        allergen_score = Allergies.allergens[item]
+        return self.score & allergen_score != 0
 
     @property
     def lst(self):
-        pass
+        return [k for k, v in Allergies.allergens.items() if self.allergic_to(k)]
