d0e0c519b632242fd02f2662d745f9dbc92594d5
3mtee <731529+3mtee@users.noreply.github.com>
2021-02-11 17:31:21 +0200

    Did the "Sum of Multiples" exercise

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,7 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    if len(multiples) == 0:
+        return 0
+    return sum(num for num in range(min(multiples), limit * max(multiples)) if
+               any(m != 0 and num % m == 0 and num < limit for m in multiples))
+
+
